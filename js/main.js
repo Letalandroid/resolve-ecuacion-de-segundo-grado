a512b6435a157d8b3ed8c785f5b1c255a63be4d5
Letalandroid <carlossoncra@protonmail.com>
2021-11-02 11:20:20 -0500

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+let resolver = function() {
+
+    let a = parseInt(document.getElementById("a").value);
+    let b = parseInt(document.getElementById("b").value);
+    let c = parseInt(document.getElementById("c").value);
+
+    let x1 = (-b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a;
+    let x2 = (-b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / 2 * a;
+
+    alert("El primer valor es: " + x1 + "\nEl segundo valor es: " + x2);
+
+}
